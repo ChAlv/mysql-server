76e3f201acc910a905ef542ff346605e651cf1eb
Julien ZHAN <julien.zhan@telecomnancy.eu>
2019-03-27 15:26:44 +0100

    Correcting fuzz

--- fuzz/fuzzQuery.cc
+++ fuzz/fuzzQuery.cc
@@ -2,12 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include <sql_main.h>
+#include "sql/sql_class.h"
+#include "sql/sql_prepare.h"
 
-extern "C" int LLVMFuzzerTestQueryInput(const uint8_t data,size_t size){
+extern "C" int LLVMFuzzerTestQueryInput(const uint8_t* data,size_t size){
 	// MYSQL *mysql;
-	int i;
-	int j = 0;
 	size_t offset = 0;
 	size_t sizereq = 0;
 	THD *thd = new THD();
@@ -19,9 +18,9 @@ extern "C" int LLVMFuzzerTestQueryInput(const uint8_t data,size_t size){
 	while (offset+sizereq < size) {
 		sizereq++;
 		if (data[offset+sizereq-1] == 0) {
-			Prepare_statement *stmt; 
+			Prepared_statement *stmt; 
 			stmt = new Prepared_statement(thd);
-			mysqld_stmt_prepare(thd,data+offset,sizereq,stmt);
+			mysqld_stmt_prepare(thd,(const char*)data+offset,sizereq,stmt);
 			delete stmt;
 			offset+=sizereq;
 			sizereq = 0;
