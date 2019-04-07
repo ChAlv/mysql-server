0ca1de03dee07d04d9ecb74a77b1abb2cfb2f996
Zouhair JANATI-IDRISSI <zouhair.janati-idrissi@telecomnancy.eu>
2019-04-07 17:27:23 +0200

    modif de la fuzz target

--- fuzz/fuzzing_stmt_prepare.cc
+++ fuzz/fuzzing_stmt_prepare.cc
@@ -18,17 +18,60 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
       mysql.options.protocol = MYSQL_PROTOCOL_FUZZ;
       sock_initfuzz(Data,Size);
 
-    if (!mysql_real_connect(&mysql,"localhost","root","root","",0,NULL,0)) {
+    if (!mysql_real_connect(&mysql,"localhost","root","root","",0,NULL,0))
+    {
           		return 0; //ce n'est pas une erreur en soi
-      	}
+    }
 
     MYSQL_STMT *stmt = mysql_stmt_init(&mysql);
-
-    if (stmt && mysql_stmt_prepare(stmt, "SELECT * FROM Cars",(ulong)strlen("SELECT * FROM Cars")))
+    if (!stmt)
     {
-    	printf("Error:%d %s\n",mysql_errno(&mysql),mysql_error(&mysql));
+      printf(stderr, " mysql_stmt_init(), out of memory\n");
+      mysql_stmt_close(stmt);
+      mysql_close(&mysql);
+      return 0;
     }
-    
+    if (mysql_stmt_prepare(stmt, "SELECT * FROM Cars",(ulong)strlen("SELECT * FROM Cars")))
+    {
+        //printf("Error:%d %s\n",mysql_errno(&mysql),mysql_error(&mysql));
+        printf(" %s\n", mysql_stmt_error(stmt));
+        mysql_stmt_close(stmt);
+        mysql_close(&mysql);
+        return 0;
+    }
+    if (mysql_stmt_execute(stmt))
+    {
+        printf(" %s\n", mysql_stmt_error(stmt));
+        mysql_stmt_close(stmt);
+        mysql_close(&mysql);
+        return 0;
+    }
+    prepare_meta_result = mysql_stmt_result_metadata(stmt);
+    if (!prepare_meta_result)
+    {
+        printf(" %s\n", mysql_stmt_error(stmt));
+        mysql_stmt_close(stmt);
+        mysql_close(&mysql);
+        return 0;
+    }
+    column_count= mysql_num_fields(prepare_meta_result);
+    MYSQL_BIND    bind[column_count];
+    memset(bind, 0, sizeof(bind));
+    if (mysql_stmt_bind_result(stmt, bind))
+    {
+      printf(" mysql_stmt_bind_result() failed\n");
+      printf(" %s\n", mysql_stmt_error(stmt));
+      mysql_free_result(prepare_meta_result);
+      mysql_stmt_close(stmt);
+      mysql_close(&mysql);
+      return 0;
+    }
+    while (!mysql_stmt_fetch(stmt))
+    {
+     }
+
+
+    mysql_free_result(prepare_meta_result);
     mysql_stmt_close(stmt);
     mysql_close(&mysql);
     
