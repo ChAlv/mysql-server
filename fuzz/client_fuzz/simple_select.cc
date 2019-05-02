bc63d493c2d7488c7c9f430c42ca243ed0dc0eec
Christopher <christopher.alves@telecomnancy.net>
2019-05-02 13:24:45 +0200

    Adding fuzz directory and modifications to support it.

---
+++ fuzz/client_fuzz/simple_select.cc
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <mysql.h>
+#include <mysql/client_plugin.h>
+#include <mysqld_error.h>
+#include "violite.h"
+
+int main() {
+  MYSQL mysql;
+  mysql_init(&mysql);
+  mysql.options.protocol = MYSQL_PROTOCOL_TCP;
+    if (!mysql_real_connect(&mysql,"localhost","root","root","",1232,NULL,0)) {
+		mysql_error(&mysql)); 
+      		return 0;
+  	}
+
+  mysql_query(&mysql, "SELECT * FROM Cars");
+  mysql_close(&mysql);
+  return 0;
+}
